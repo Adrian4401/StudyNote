cf4e3cb877758618dfae40cba86345ddaa608741
Adrian4401 <adrianzakrzewski4401@gmail.com>
2024-02-27 19:17:56 +0100

    DropdownPickers working good without error.

--- screens/NoteScreen.js
+++ screens/NoteScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, ScrollView, TouchableOpacity, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import DropDownPicker from 'react-native-dropdown-picker';
@@ -11,20 +11,6 @@ import { globalStyles } from '../styles/globalStyles';
 import { dropdownStyles } from '../styles/dropdownStyles'
 
 import { Ionicons, FontAwesome5, Feather, MaterialIcons } from '@expo/vector-icons';
-
-const subjectsList = [
-  { label: 'Wszystkie przedmioty', value: '0' },
-  { label: 'Wizualizacja 3D', value: '1' },
-  { label: 'Sztuczna Inteligencja', value: '2' },
-  { label: 'Programowanie komponentowe', value: '3' },
-  { label: 'Język angielski', value: '4' }
-];
-
-const timeList = [
-  { label: 'Najnowsze', value: '0' },
-  { label: 'Najstarsze', value: '1' },
-  { label: 'Ostatni tydzień', value: '2' }
-];
 
 export default function NoteScreen() {
 
@@ -43,9 +29,71 @@ export default function NoteScreen() {
   const [newest, setNewest] = useState([
     {label: 'Najnowsze', value: '0'},
     {label: 'Najstarsze', value: '1'},
-    {label: 'Ostatni tydzień', value: '1'}
+    {label: 'Ostatni tydzień', value: '2'}
   ]);
 
+  const renderItem = ({ item }) => {
+    if (item.type === 'header') {
+      return (
+        <View style={globalStyles.headlineViewWithIcon}>
+          <Text style={globalStyles.headlineText}>Twoje notatki</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('AddNoteScreen')}>
+            <Feather name="plus" size={26} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      );
+    } else if (item.type === 'dropdown') {
+      return (
+        <View style={{width: '100%', marginBottom: 30}}>
+          <DropDownPicker
+            open={openSubjects}
+            value={valueSubjects}
+            items={subjects}
+            setOpen={setOpenSubjects}
+            setValue={setValueSubjects}
+            setItems={setSubjects}
+            ScrollView={false}
+            zIndex={10000}
+            style={{...styles.style, marginBottom: 10}}
+            dropDownContainerStyle={styles.dropDownContainerStyle}
+            textStyle={styles.textStyle}
+            arrowIconContainerStyle={styles.arrowIconContainerStyle}
+          />
+
+          <DropDownPicker
+            open={openNewest}
+            value={valueNewest}
+            items={newest}
+            setOpen={setOpenNewest}
+            setValue={setValueNewest}
+            setItems={setNewest}
+            ScrollView={false}
+            style={styles.style}
+            dropDownContainerStyle={styles.dropDownContainerStyle}
+            textStyle={styles.textStyle}
+            arrowIconContainerStyle={styles.arrowIconContainerStyle}
+          />
+        </View>
+      );
+    } else if (item.type === 'note') {
+      return (
+        <View style={{...globalStyles.eventView, padding: 15, borderColor: MyColors.appLightGray, borderWidth: 1, zIndex: 1}}>
+          <View>
+            <Text style={globalStyles.headlineText}>Przetwarzanie informacji</Text>
+          </View>
+          <View style={{flex: 1, backgroundColor: MyColors.appGray, height: 1}} />
+          <View style={globalStyles.eventSubjectView}>
+            <FontAwesome5 name="book" size={20} color="#fff"/>
+            <Text style={globalStyles.subjectText}>Sztuczna inteligencja</Text>
+          </View>
+          <View style={globalStyles.eventDatetimeView}>
+              <Ionicons name="calendar-clear" size={18} color='#D1D0D0' style={{marginRight: 10}} />
+              <Text style={globalStyles.littleText}>12.01.2024</Text>
+          </View>
+        </View>
+      );
+    }
+  };
 
   return (
     <>
@@ -60,11 +108,30 @@ export default function NoteScreen() {
         </View>
 
 
+        
+
         {/* CONTAINER */}
-        <ScrollView>
+        <View style={styles.container}>
+          <FlatList
+            inverted data={[
+              { type: 'note' },
+              { type: 'note' },
+              { type: 'note' },
+              { type: 'dropdown' },
+              { type: 'header' }
+            ]}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingTop: 20}}
+            style={{paddingBottom: 20}}
+          />
+        </View>
+        
+        {/* <ScrollView>
           <View style={styles.container}>
             
-            {/* HEADLINE */}
+            HEADLINE
             <View style={globalStyles.headlineViewWithIcon}>
               <Text style={globalStyles.headlineText}>Twoje notatki</Text>
               <TouchableOpacity onPress={() => navigation.navigate('AddNoteScreen')}>
@@ -83,24 +150,10 @@ export default function NoteScreen() {
                 setItems={setSubjects}
                 ScrollView={false}
                 zIndex={10000}
-                style={{
-                  backgroundColor: MyColors.appDark,
-                  borderWidth: 1,
-                  borderColor: MyColors.appOrange,
-                  marginBottom: 10
-                }}
-                dropDownContainerStyle={{
-                  backgroundColor: MyColors.appDark,
-                  borderWidth: 1,
-                  borderColor: MyColors.appOrange
-                }}
-                textStyle={{
-                  color: MyColors.appLightGray
-                }}
-                arrowIconContainerStyle={{
-                  backgroundColor: MyColors.appOrange,
-                  borderRadius: 5
-                }}
+                style={styles.style}
+                dropDownContainerStyle={styles.dropDownContainerStyle}
+                textStyle={styles.textStyle}
+                arrowIconContainerStyle={styles.arrowIconContainerStyle}
               />
 
               <DropDownPicker
@@ -111,59 +164,16 @@ export default function NoteScreen() {
                 setValue={setValueNewest}
                 setItems={setNewest}
                 ScrollView={false}
-                style={{
-                  backgroundColor: MyColors.appDark,
-                  borderWidth: 1,
-                  borderColor: MyColors.appOrange
-                }}
-                dropDownContainerStyle={{
-                  backgroundColor: MyColors.appDark,
-                  borderWidth: 1,
-                  borderColor: MyColors.appOrange
-                }}
-                textStyle={{
-                  color: MyColors.appLightGray
-                }}
-                arrowIconContainerStyle={{
-                  backgroundColor: MyColors.appOrange,
-                  borderRadius: 5
-                }}
+                style={styles.style}
+                dropDownContainerStyle={styles.dropDownContainerStyle}
+                textStyle={styles.textStyle}
+                arrowIconContainerStyle={styles.arrowIconContainerStyle}
               />
 
              
             </View>
 
-            {/* NOTE */}
-
-            <View style={{...globalStyles.eventView, padding: 15, borderColor: MyColors.appLightGray, borderWidth: 1}}>
-              <View>
-                <Text style={globalStyles.headlineText}>Przetwarzanie informacji</Text>
-              </View>
-              <View style={{flex: 1, backgroundColor: MyColors.appGray, height: 1}} />
-              <View style={globalStyles.eventSubjectView}>
-                <FontAwesome5 name="book" size={20} color="#fff"/>
-                <Text style={globalStyles.subjectText}>Sztuczna inteligencja</Text>
-              </View>
-              <View style={globalStyles.eventDatetimeView}>
-                  <Ionicons name="calendar-clear" size={18} color='#D1D0D0' style={{marginRight: 10}} />
-                  <Text style={globalStyles.littleText}>12.01.2024</Text>
-                </View>
-            </View>
-
-            <View style={{...globalStyles.eventView, padding: 15, borderColor: MyColors.appLightGray, borderWidth: 1}}>
-              <View>
-                <Text style={globalStyles.headlineText}>Przetwarzanie informacji</Text>
-              </View>
-              <View style={{flex: 1, backgroundColor: MyColors.appGray, height: 1}} />
-              <View style={globalStyles.eventSubjectView}>
-                <FontAwesome5 name="book" size={20} color="#fff"/>
-                <Text style={globalStyles.subjectText}>Sztuczna inteligencja</Text>
-              </View>
-              <View style={globalStyles.eventDatetimeView}>
-                  <Ionicons name="calendar-clear" size={18} color='#D1D0D0' style={{marginRight: 10}} />
-                  <Text style={globalStyles.littleText}>12.01.2024</Text>
-                </View>
-            </View>
+            NOTE
 
             <View style={{...globalStyles.eventView, padding: 15, borderColor: MyColors.appLightGray, borderWidth: 1}}>
               <View>
@@ -182,9 +192,9 @@ export default function NoteScreen() {
 
 
           </View>
-        </ScrollView>
+        </ScrollView> */}
 
-        <View style={{width: '100%',height: 40}} />
+        {/* <View style={{width: '100%',height: 40}} /> */}
 
       </SafeAreaView>
     </>
@@ -196,7 +206,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: MyColors.appBackground,
     alignItems: 'center',
-    padding: 20,
-    paddingBottom: 120
+    paddingHorizontal: 20,
+    paddingBottom: 50
+  },
+  style: {
+    backgroundColor: MyColors.appDark,
+    borderWidth: 1,
+    borderColor: MyColors.appOrange
+  },
+  dropDownContainerStyle: {
+    backgroundColor: MyColors.appDark,
+    borderWidth: 1,
+    borderColor: MyColors.appOrange
+  },
+  textStyle: {
+    color: MyColors.appLightGray
+  },
+  arrowIconContainerStyle: {
+    backgroundColor: MyColors.appOrange,
+    borderRadius: 5
   }
 });
